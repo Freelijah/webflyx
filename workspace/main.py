52d2c4077b4697995e6c286036d8ba7cfc0ce516
Freelijah <camelblues96@outlook.com>
2025-07-03 15:48:35 -0500

    Probably messed up this too

---
+++ workspace/main.py
@@ -0,0 +1,37 @@
+import sys
+from stats import (
+        get_num_words,
+        chars_dict_to_sorted_list,
+        get_chars_dict
+)
+
+def main():
+	if len(sys.argv) < 2:
+		print("Usage: python3 main.py <path_to_book>")
+		sys.exit(1)
+	filepath = sys.argv[1]
+
+	book_text = get_book_text(sys.argv[1])
+	num_words = get_num_words(book_text)
+	chars_dict = get_chars_dict(book_text)
+	chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+	print_report(book_text, sys.argv[1], num_words, chars_sorted_list)
+
+def get_book_text(filepath):
+	with open(filepath) as f:
+	    return f.read()
+
+def print_report(book_text, filepath, num_words, chars_sorted_list):
+    	print("============ BOOKBOT ============")
+    	print(f"Analyzing book found at {filepath}...")
+    	print("----------- Word Count ----------")
+    	print(f"Found {num_words} total words")
+    	print("--------- Character Count -------")
+    	for item in chars_sorted_list:
+            if not item["char"].isalpha():
+            	continue
+            print(f"{item['char']}: {item['num']}")
+
+    	print("============= END ===============")
+
+main()
